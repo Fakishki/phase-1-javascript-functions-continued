a1669462ba30b2011ebc7433a449012b3d4d2e2b
Wally Pfingsten <wallypfingsten@gmail.com>
2023-02-22 10:02:10 -0800

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return (`This Saturday, I want to ${activity}!`);
+}
+
+saturdayFun();
+
+const mondayWork = function (job = "go to the office") {
+    return (`This Monday, I will ${job}.`);
+}
+
+mondayWork();
+
+function wrapAdjective(outerParameter = `*`) {
+    const innerFunction = function (thing = `special`) {
+        return `You are ${outerParameter}${thing}${outerParameter}!`;
+    };
+    return innerFunction;
+}
+
+wrapAdjective("%")("a dedicated programmer");
